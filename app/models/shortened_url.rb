47c6cddf054fd21e6d6a6437730006930f4f66a6
Jeison Arenales <jeison.arenales@alkanza.us>
2019-07-06 15:37:52 -0500

    Handling response for top action

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,3 +1,8 @@
 class ShortenedUrl < ApplicationRecord
   validates :url, url: true
+
+  # handle json response
+  def as_json(options={})
+    super(:only => [:url,:title, :unique_key])
+  end
 end
